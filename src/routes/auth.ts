eec1511207b594a3756b6440e4a30469cc924cb0
Marc <marcfp2001@gmail.com>
2021-12-26 21:44:57 +0100

    middlewares

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,6 +4,8 @@ import { validEmail, validLength } from '../middlewares/validators';
 
 const router = Router();
 
+router.use(validEmail);
+router.use(validLength(4))
 router.post('/signin', Auth.signin);
 router.post('/signup', Auth.signup);
 
